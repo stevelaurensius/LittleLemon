771b9d7da31d481e99d0d07e5dca4a7086c0b44e
stevelaurensius <steve.laurensius+github@gmail.com>
2024-10-16 03:07:37 +0700

    first commit

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Menu
+from .serializers import bookingSerializer, menuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
